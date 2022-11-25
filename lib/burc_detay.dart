8cb6fafc4453a7fb746a9ed9a1f2edd3d2e28f5f
receponalan <rekouno86@gmail.com>
2022-11-25 22:31:19 +0300

    palette Generator eklendi

--- lib/burc_detay.dart
+++ lib/burc_detay.dart
@@ -1,42 +1,63 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ilk_proje/model/burc.dart';
+import 'package:palette_generator/palette_generator.dart';
 
-class BurcDetay extends StatelessWidget {
+class BurcDetay extends StatefulWidget {
   final Burc secilenBurc;
-  const BurcDetay({super.key, required this.secilenBurc});
+  const BurcDetay({required this.secilenBurc, Key? key}) : super(key: key);
+
+  @override
+  _BurcDetayState createState() => _BurcDetayState();
+}
+
+class _BurcDetayState extends State<BurcDetay> {
+  Color appbarRengi = Colors.pink;
+  late PaletteGenerator _generator;
+
+  @override
+  void initState() {
+    super.initState();
+    appbarRenginiBul();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            expandedHeight: 250,
-            pinned: true,
-            backgroundColor: Colors.pink,
-            flexibleSpace: FlexibleSpaceBar(
-              title: Text(secilenBurc.burcAdi + ' Burcu ve Özellikleri'),
-              centerTitle: true,
-              background: Image.asset(
-                'images/' + secilenBurc.burcBuyukResim,
-                fit: BoxFit.cover,
+        body: CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          expandedHeight: 250,
+          pinned: true,
+          backgroundColor: appbarRengi,
+          flexibleSpace: FlexibleSpaceBar(
+            title: Text(widget.secilenBurc.burcAdi + " Burcu ve Özellikleri"),
+            centerTitle: true,
+            background: Image.asset(
+              'images/' + widget.secilenBurc.burcBuyukResim,
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        SliverToBoxAdapter(
+          child: Container(
+            margin: EdgeInsets.all(16),
+            padding: EdgeInsets.all(8),
+            child: SingleChildScrollView(
+              child: Text(
+                widget.secilenBurc.burcDetayi,
+                style: Theme.of(context).textTheme.subtitle1,
               ),
             ),
           ),
-          SliverToBoxAdapter(
-            child: Container(
-              margin: EdgeInsets.all(16),
-              padding: EdgeInsets.all(8),
-              child: SingleChildScrollView(
-                child: Text(
-                  secilenBurc.burcDetayi,
-                  style: Theme.of(context).textTheme.subtitle1,
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
+        ),
+      ],
+    ));
+  }
+
+  void appbarRenginiBul() async {
+    _generator = await PaletteGenerator.fromImageProvider(
+        AssetImage('images/${widget.secilenBurc.burcBuyukResim}'));
+    appbarRengi = _generator.vibrantColor!.color;
+    setState(() {});
   }
 }
